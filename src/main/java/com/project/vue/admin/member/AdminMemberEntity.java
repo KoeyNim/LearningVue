a3f438eafdb76f67c7824cb95318a0d97e3082a2
이민혁 <49alswjd@nullsoft.co.kr>
2022-06-03 16:52:48 +0900

    관리자 데이터 접근 패키지 추가

---
+++ src/main/java/com/project/vue/admin/member/AdminMemberEntity.java
@@ -0,0 +1,57 @@
+package com.project.vue.admin.member;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.project.vue.common.StringCryptoConverter;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "member")
+public class AdminMemberEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "memberId")
+	private Long id;
+
+	@NotNull
+	@Column(columnDefinition = "varchar(255)")
+	@Convert(converter = StringCryptoConverter.class)
+	private String userId;
+	
+	@NotNull
+	@Column(columnDefinition = "varchar(255)")
+	@Convert(converter = StringCryptoConverter.class)
+	private String userName;
+	
+	@NotNull
+	@Column(columnDefinition = "varchar(255)")
+	private String userPwd;
+	
+	@NotNull
+	private Integer age;
+	
+	@NotNull
+	@Column(columnDefinition = "varchar(255)")
+	@Convert(converter = StringCryptoConverter.class)
+	private String email;
+	
+	@NotNull
+	@Column(columnDefinition = "varchar(4)")
+	private String gender;
+	
+	@NotNull
+	@Column(columnDefinition = "varchar(255)")
+	@Convert(converter = StringCryptoConverter.class)
+	private String phone;
+
+}
---
+++ src/main/java/com/project/vue/admin/member/AdminMemberRepository.java
@@ -0,0 +1,10 @@
+package com.project.vue.admin.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import com.project.vue.admin.post.AdminPostEntity;
+
+public interface AdminMemberRepository extends JpaRepository<AdminMemberEntity, Long>, JpaSpecificationExecutor<AdminPostEntity> {
+	AdminMemberEntity findByUserId(String userId);
+}
