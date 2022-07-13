33def744f3228521a2ce726dc53b149eec43ab09
이민혁 <49alswjd@nullsoft.co.kr>
2022-07-13 10:57:40 +0900

    사용하지 않는 테스트 코드 정리

--- src/main/java/com/project/vue/member/MemberService.java
+++ src/main/java/com/project/vue/member/MemberService.java
@@ -3,14 +3,11 @@ package com.project.vue.member;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.project.vue.role.RoleEntity;
 import com.project.vue.role.RoleEnum;
 import com.project.vue.role.RoleRepository;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -22,15 +19,8 @@ public class MemberService {
 	private final BCryptPasswordEncoder passwordEncoder;
     
     public MemberEntity save(MemberEntity member) {
-    	RoleEntity roleEntity = new RoleEntity();
-    	roleEntity.setRoleKey(RoleEnum.USER.getRoleKey());
-    	
-    	log.debug("member {}", member);
-    	log.debug("roleEntity {}", roleEntity);
-    	
     	member.setUserPwd(passwordEncoder.encode(member.getUserPwd()));
     	member.setRole(roleRepository.findByRoleKey(RoleEnum.USER.getRoleKey()));
-    	log.debug("member {}", member);
     	return memberRepository.save(member);
     }
     
